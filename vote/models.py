39bf237e630100f52a9cddbb5e8df50823c017a2
threecatsago <3170440153@qq.com>
2023-09-23 16:14:46 +0800

    add vote model

--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
 # Create your models here.
+
+class Question(models.Model):
+    questions_text = models.TextField(max_length=200)
+    pub_time = models.DateTimeField('date publish')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.TextField(max_length=200)
+    votes = models.IntegerField(default=0)
