128778de499a28aaa4ee684c76739fc94242449c
threecatsago <3170440153@qq.com>
2023-09-27 18:38:36 +0800

    add html and more views with urls and meet BUGGGG!!!!

--- vote/urls.py
+++ vote/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-
+app_name = 'vote'
 urlpatterns = [path("", views.index, name = "index"),
-               path('<int:question_id>', views.detail, name = "index"),
+               path('<int:question_id>', views.detail, name = "detail"),
                path("<int:question_id>/results/",views.results,name = "results"),
-               path("<int:question_id>/voting/",views.voting, name = "vote")
+               path("<int:question_id>/voting/",views.voting, name = "voting")
                ]
--- vote/views.py
+++ vote/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 from .models import Question
+
 # Create your views here.
 
 def index_origin(request):
@@ -31,9 +33,19 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    results = render(request , "vote/results.html",{'question':question})
+    return results
 
 
 def voting(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST["choice"])
+    except:
+        return render(request, 'vote/detail.html',{'question':question,'error_message': "You didn't select a choice.",})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("vote:results"),args = (question.id,))
+
